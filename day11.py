f53de31ddb86c1eb2f8b37e79530645224bfef66
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-07-10 23:15:44 +0600

    string added

--- day11.py
+++ day11.py
@@ -6,3 +6,10 @@ Free mind to share my knowledge i have gather in my life
 I write code to make the world a better place
 """
 print(b)
+
+c = "Rejwan"
+
+# using for loop to pint the string.
+# use can treat string as array in python. string is a set of character
+for text in c:
+    print(text)
