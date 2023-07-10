5fa29f6ec906e96b995e4ed14f635dc9599159fa
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-07-10 21:26:21 +0600

    day 9 added

---
+++ day9.py
@@ -0,0 +1,25 @@
+# typecasting in python
+
+# there are two types of typecasting in python
+# 1. implicit type casting ---------- when python change the datatype of a variable automatically is called implicit type casting
+# 2 . explicit type casting --------- when a developer change the datatype of a variable according to the need then it called explicit data type
+
+# implicit datatype
+
+a = 8
+b = 1.9
+
+print(a+b)
+print("\n")
+
+# here python automatically change the datatype of a into integer to float and print the data in float
+
+
+# explicit datatype
+
+c = "4"
+d = 5
+
+print(int(c)+d)
+
+# here c is a string and developer change the datatype of the variable into integer to sum two variable
