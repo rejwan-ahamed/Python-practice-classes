0e3ea7bc1504c7d6defb9ff5ebed02347e9f916c
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-07-10 21:06:11 +0600

    test upload

---
+++ day5.py
@@ -0,0 +1,4 @@
+print("Hay it's rejwan from pase. \nHi it's rejwan from future \nNow I am a successful python engineer")
+
+
+# in this program \n is a scape sequence character
---
+++ day6.py
@@ -0,0 +1,30 @@
+# the code is about different of data types
+# ues of type() function. this function tell us the data type of a variable
+
+a = 6
+b = "Rejwan"
+c = 5.44
+d = True
+
+print("the type of a is: ", type(a))
+print("the type of b is: ", type(b))
+print("the type of c is: ", type(c))
+print("the type of d is: ", type(d))
+
+# new data types for me
+# 1. list ------- kind of array
+# 2. tuple ------ its kind of array but it's data can't be change means it is immutable. can be identify (). it use first brackets
+# 3. dictionary -------- we call it in javascript object. But here in python we called it dictionary it's operations are same like object.
+
+# array
+
+r = [2, 4, 5, 6]
+
+
+# tuple
+
+t = ("j1", "j2", "j3", "jack jack")
+
+# dictionary
+
+s = {"name": "Rejwan", "age": 22, "python start date": 2, "successful": True}
