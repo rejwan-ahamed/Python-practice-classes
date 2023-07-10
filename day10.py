47b0660b7f42fda542ff960e14d2782f01dc57be
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-07-10 21:47:02 +0600

    day 10 added

---
+++ day10.py
@@ -0,0 +1,8 @@
+# uses of input function in python.
+# this input() function is used for get input from user and this function convert user input into string
+
+a = input("please enter your name: ")
+print(a)
+
+b = input("enter a number: ")
+print("your entered number is :", b + "\nThe datatype of it is :", type(b))
