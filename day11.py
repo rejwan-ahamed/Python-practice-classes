08ee9a8c5096a6fac652da2242f5be4e29b9e05f
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-07-10 22:00:53 +0600

    day 11 added

---
+++ day11.py
@@ -0,0 +1,8 @@
+# string
+
+a = "hello"
+b = """Hello i am Rejwan Ahamed from another world
+Free mind to share my knowledge i have gather in my life
+I write code to make the world a better place
+"""
+print(b)
