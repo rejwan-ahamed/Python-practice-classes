5aba4b52c3505d519ae9824f9b0628adfa20356e
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-08-06 22:14:08 +0600

    day 14 added

---
+++ day14.py
@@ -0,0 +1,11 @@
+# conditions is python
+
+a = int(input("please enter your age: "))
+if (a > 20):
+    print("you are ready to drive")
+
+elif (a == 20):
+    print("you are perfect to drive")
+
+else:
+    print("you are not ready to drive")
