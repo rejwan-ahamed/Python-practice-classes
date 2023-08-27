380bea74d8f0b91a3387da4a776e793c66462ef4
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-08-27 23:01:25 +0600

    day 11 edited

--- day11.py
+++ day11.py
@@ -5,11 +5,17 @@ b = """Hello i am Rejwan Ahamed from another world
 Free mind to share my knowledge i have gather in my life
 I write code to make the world a better place
 """
+
+c= '''I am rejwan. Focus on your goal and don't be distract.
+just go on the flow and I am sure you will be successful just do it'''
+
+print(c)
+print('\n')
 print(b)
 
 c = "Rejwan"
 
-# using for loop to pint the string.
+# using for loop to pint the string. we use for in loop because string is kind of array
 # use can treat string as array in python. string is a set of character
 for text in c:
     print(text)
