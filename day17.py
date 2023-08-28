ea44ed497c2846d33ebcecf045111c45416000c4
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-08-28 23:18:01 +0600

    day 17 added

---
+++ day17.py
@@ -0,0 +1,12 @@
+# for in loop uses
+
+colors = ['red', 'green', 'blue', 'yellow', 'violet', 'purple']
+
+for color in colors:
+    print("\n")
+    print(color)
+    print("\n")
+    # printing each string of the color name using for loop in the in color name
+
+    for i in color:
+        print(i)
