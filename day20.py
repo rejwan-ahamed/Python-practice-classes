5785c749be73941d0db098052a5251166ba3428b
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-08-29 12:34:39 +0600

    day 20 added

---
+++ day20.py
@@ -0,0 +1,14 @@
+def sum(a, b):
+    total = a+b
+    return total
+
+
+# this subtraction is made but it is a empty function.
+# this function will do nothing and will not return anything
+# we can create empty function in python
+# also we can use the function later for this use use pass argument inside the function to prevent the error
+def subtraction(a, b):
+    pass
+
+
+print(sum(10, 20))
