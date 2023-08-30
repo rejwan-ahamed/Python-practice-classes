5f3a4e0addea2d7d2e62a470eed419aacaf7cfbd
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-08-30 22:22:45 +0600

    day 22 added

---
+++ day22.py
@@ -0,0 +1,12 @@
+# array is python
+
+names = ['rahi', 'raisa','maruf', 'rabbi','elite']
+
+print(names[-3])
+
+# what the code is doing we can break it down into 2 stapes
+# 1. we need to first get the length of the list or array
+# 2. then we need to subtract the negative index from the length of the array
+# bellow I am explaining the code 
+
+print(names[len(names)-3])
