501c3198e8ac8f2b61ebd5ed7ef872a7f37838d5
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-08-30 21:40:43 +0600

    day 21 added

--- day21.py
+++ day21.py
@@ -3,6 +3,17 @@
 def defaultArgument(a, b=3):
     sum = a+b
     print(sum)
-
-
 defaultArgument(3)
+
+
+# if we want to get many number of average but dont know how many numbers we will get in the input
+# here ue are taking a tuple as an argument and the syntax is *numbers we use * to define a tuple for a function argument
+
+def average(*numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    print("Average is :", sum/len(numbers))
+
+
+average(2, 4, 5, 30, 50)
