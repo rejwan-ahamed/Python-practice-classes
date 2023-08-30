6af8af28780b13f0e1210fc5d1f697a7eb429504
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-08-30 23:29:54 +0600

    day 25 added

---
+++ day25.py
@@ -0,0 +1,9 @@
+tup = (3, 43, 4, 34, 34, 2, 23, 'rejwan', True)
+listTuple = list(tup)
+print(listTuple)
+
+listTuple.append('supra')
+print(listTuple)
+
+convertTuple = tuple(listTuple)
+print(convertTuple)
