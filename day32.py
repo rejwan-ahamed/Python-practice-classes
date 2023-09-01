81ff6e09a28993bad33da9ec1bd010c3d0531599
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-09-01 12:30:55 +0600

    day 32 added

---
+++ day32.py
@@ -0,0 +1,10 @@
+# set datatype in python
+# set is a datatype in python which dose not contain duplicate data
+# it return only one set of data
+
+
+numbers = {2, 33, 52, 2, 5, 7, 5, 7}
+print(numbers)
+
+number = set()
+print(type(numbers))
